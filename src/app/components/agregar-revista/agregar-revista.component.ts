e2f47ff322367f62ebf3c2e464bea6dc8c44c2a6
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-04-10 21:59:26 -0500

    solucion

--- src/app/components/agregar-revista/agregar-revista.component.ts
+++ src/app/components/agregar-revista/agregar-revista.component.ts
@@ -3,6 +3,15 @@ import { AppMaterialModule } from '../../app.material.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../menu/menu.component';
+import { Pais } from '../../models/pais.model';
+import { DataCatalogo } from '../../models/dataCatalogo.model';
+import { UtilService } from '../../services/util.service';
+import { Revista } from '../../models/revista.model';
+import { TokenService } from '../../security/token.service';
+import { Usuario } from '../../models/usuario.model';
+import { RevistaService } from '../../services/revisa.service';
+
+import Swal from 'sweetalert2'
 
 @Component({
   standalone: true,
@@ -13,4 +22,45 @@ import { MenuComponent } from '../../menu/menu.component';
 })
 export class AgregarRevistaComponent {
 
+      lstPais: Pais[] = [];
+      lstTipo: DataCatalogo[] = [];
+
+      objRevista: Revista ={
+            nombre: "",
+            frecuencia: "",
+            fechaCreacion : new Date(),
+            telefono: "",
+            pais:{
+              idPais:-1
+            },
+            tipoRevista:{
+                idDataCatalogo:-1
+            }
+        }
+        objUsuario: Usuario = {} ;
+
+      constructor(private utilService: UtilService, 
+                  private tokenService: TokenService,
+                  private revistaService: RevistaService){
+              this.utilService.listaTipoLibroRevista().subscribe(
+                    x =>  this.lstTipo = x
+              );
+              this.utilService.listaPais().subscribe(
+                x =>  this.lstPais = x
+              );
+              this.objUsuario.idUsuario = tokenService.getUserId();
+      }
+      registra(){
+            this.objRevista.usuarioActualiza = this.objUsuario;
+            this.objRevista.usuarioRegistro = this.objUsuario;
+            this.revistaService.registrar(this.objRevista).subscribe(
+              x=>{
+                Swal.fire({
+                  icon: 'info',
+                  title: 'Resultado del Registro',
+                  text: x.mensaje,
+                })
+              },
+            );
+      }
 }
