853cbbc2d86ae7d3aacc80df2588051c0065350c
William Rivera <riverawilliam2011@gmail.com>
2024-06-13 22:02:26 -0500

    PC1 completo

--- src/app/models/monto-prestamo.model.ts
+++ src/app/models/monto-prestamo.model.ts
@@ -1,2 +1,13 @@
+import { DataCatalogo } from './dataCatalogo.model';
+
 export class MontoPrestamo {
+  idMontoPrestamo?: number;
+  capital?: number;
+  dias?: DataCatalogo;
+  monto?: number;
+  estado?: number;
+  fechaRegistro?: string;
+  fechaActualizacion?: string;
+  usuarioRegistro?: number;
+  usuarioActualiza?: number;
 }
